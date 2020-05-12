65da371039883db2bed64da83307383a86f1e956
GandzyTM <gandzy1984@gmail.com>
2020-05-12 18:38:50 +0500

    HW6 DZ Game fixed

--- hw6_dz/js/game.js
+++ hw6_dz/js/game.js
@@ -2,16 +2,16 @@ const numDivs = 36;
 const maxHits = 10;
 
 let hits = 0;
+let missHits = 0;
 let firstHitTime = 0;
 
 function round() {
-  // FIXME: надо бы убрать "target" прежде чем искать новый
+  $(".game-field").removeClass("target");
+  $(".game-field").removeClass("miss");
 
   let divSelector = randomDivId();
   $(divSelector).addClass("target");
-  // TODO: помечать target текущим номером
-
-  // FIXME: тут надо определять при первом клике firstHitTime
+  $(".target").text(hits + 1);
 
   if (hits === maxHits) {
     endGame();
@@ -19,32 +19,44 @@ function round() {
 }
 
 function endGame() {
-  // FIXME: спрятать игровое поле сначала
+  $('#game-fields').addClass("d-none");
 
   let totalPlayedMillis = getTimestamp() - firstHitTime;
   let totalPlayedSeconds = Number(totalPlayedMillis / 1000).toPrecision(3);
   $("#total-time-played").text(totalPlayedSeconds);
+  let totalPoints = hits + missHits;
+  $("#total-fails").text(" " + totalPoints);
 
-  $("#win-message").removeClass("d-none");
+  $("#end-game-message").removeClass("d-none");
 }
 
 function handleClick(event) {
-  // FIXME: убирать текст со старых таргетов. Кажется есть .text?
   if ($(event.target).hasClass("target")) {
-    hits += 1;
+    hits = hits + 1;
+    $(event.target).text("");
     round();
-  }
-  // TODO: как-то отмечать если мы промахнулись? См CSS класс .miss
+  } else {
+    $(event.target).addClass("miss");
+    missHits = missHits - 1;
+  };
 }
 
 function init() {
-  // TODO: заказчик просил отдельную кнопку, запускающую игру а не просто по загрузке
   round();
-
   $(".game-field").click(handleClick);
-  $("#button-reload").click(function() {
+
+  $("#start-game-btn").click(function(){
+    firstHitTime = getTimestamp();
+    $("#game-fields").removeClass('d-none');
+    $("#start-game-btn").hide();
+  
+    $("#reload-btn").removeClass('d-none');
+  });
+
+  $("#reload-btn").click(function() {
     location.reload();
   });
+
 }
 
 $(document).ready(init);
